eaf4b2aabb63ee118aeb4feea207b632ddb0863d
Olivier Nicole <olivier.nicolesoitnicoulaz@heig-vd.ch>
2018-05-23 10:03:05 +0200

    First steps...

--- docker/client/src/client.js
+++ docker/client/src/client.js
@@ -1,6 +1,46 @@
 var protocol = require('./protocol');
 
-const http = require('http');
+var moment = require('moment');
+
+var request = require('request');
+
+request.get('http://' + protocol.IP + ':' + protocol.PORT, function(error , response, body){
+    if(error)
+        if(res.statusCode !== 200 ) console.log('Error :') //etc
+
+    //Request body..
+    console.log("GET REQUEST : ");
+    console.log(body);
+});
+
+
+var data = {
+    json: { time: moment().add(2, 'hours') }
+}
+/*
+request.post('http://' + protocol.IP + ':' + protocol.PORT, data, function(error, response, body){
+    if(error) //TODO: handle err
+        if(response.statusCode !== 200 ) console.log('Error :') //etc
+
+    //Request body..
+    console.log("POST REQUEST : ");
+    console.log(body);
+});
+*/
+
+request.post({
+    url: 'http://' + protocol.IP + ':' + protocol.PORT,
+    body: data,
+    json: true
+}, function(error, response, body){
+    console.log("POST REQUEST : ");
+    console.log(body);
+});
+
+
+
+/* the old one...
+const http = require('http')
 
 //get request
 http.get('http://' + protocol.IP + ':' + protocol.PORT, (resp) => {
@@ -15,7 +55,26 @@ http.get('http://' + protocol.IP + ':' + protocol.PORT, (resp) => {
     resp.on('end', () => {
         console.log(data);
     });
-
 }).on("error", (err) => {
     console.log("Error ! : " + err.message);
 });
+
+//post request
+http.post('http://' + protocol.IP + ':' + protocol.PORT,
+    { json: { newTime: moment().add(500) } },
+    (resp) => {
+    let data = '';
+
+    //a chunk of a data is received
+    resp.on('data', (chunk) => {
+        data += chunk;
+    });
+
+    //the response is received, show it to the terminal
+    resp.on('end', () => {
+        console.log(data);
+    });
+}).on("error", (err) => {
+    console.log("Error ! : " + err.message);
+});
+*/
--- docker/server/src/server.js
+++ docker/server/src/server.js
@@ -1,8 +1,15 @@
 var protocol = require('./protocol');
 
 var moment = require('moment');
+var bodyParser = require('body-parser');
 var express = require('express');
 var srv = express();
+
+var date = new Date();
+var offset = 0;
+
+srv.use(bodyParser.urlencoded({ extended: false }));
+srv.use(bodyParser.json());
 
 console.log("Waiting connection on port : " + protocol.PORT);
 
@@ -10,6 +17,22 @@ console.log("Waiting connection on port : " + protocol.PORT);
 srv.get('/', (request, response) => {
     response.json({
         "Current time": moment().format('LTS')
+    });
+})
+
+//when someone get /, return the current time
+srv.post('/', (request, response) => {
+
+    console.log("POST : " + moment(request.body.json.time).format('LTS'));
+
+    date = moment().set({
+        'hour': moment(request.body.json.time).format('hh'),
+        'minute': moment(request.body.json.time).format('mm'),
+        'second': moment(request.body.json.time).format('ss')
+    });
+
+    response.json({
+        "Current time": d1.format('LTS')
     });
 })
 
